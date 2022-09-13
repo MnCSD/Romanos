b41b89c707593a5fa35c2ce7b7ac2465ca875515
MnCSD <mnikolopoylos@gmail.com>
2022-09-13 21:09:59 +0300

    first commit

--- pages/services.js
+++ pages/services.js
@@ -48,6 +48,10 @@ const Services = () => {
         </p>
       </div>
 
+      <p className="text-md my-6 font-bold text-center">
+        Want to learn each of my moves first hand?
+      </p>
+
       <div className="flex justify-center items-center mt-6 space-x-2">
         <input
           value={email}
@@ -60,10 +64,30 @@ const Services = () => {
       <div className="flex justify-center items-center mt-6">
         <button
           onClick={submitEmail}
-          className="px-14 transition-[2s] py-4 bg-green-600 cursor-pointer rounded-full text-white font-bold hover:bg-white hover:text-green-600 hover:border-2 hover:border-green-600 outline-none"
+          className="px-14 duration-1000 py-4 bg-green-600 cursor-pointer rounded-full text-white font-bold hover:bg-white hover:text-green-600 hover:border-2 hover:border-green-600 outline-none hover:  scale-110"
         >
           Submit
         </button>
+      </div>
+
+      <div className="flex flex-col space-y-10 justify-center items-center mt-10">
+        <h1 className="text-3xl text-green-600 uppercase font-semibold">
+          payment
+        </h1>
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl text-black uppercase">PAYPAL EMAIL</h2>
+          <a
+            href="mailto:romanoszervos@gmail.com"
+            className="text-green-600 underline"
+          >
+            romanoszervos@gmail.com
+          </a>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl text-black uppercase">PAYPAL MERCHANT ID</h2>
+          <h2 className="text-2xl text-black uppercase">9YFANEXYMK9JY</h2>
+        </div>
       </div>
 
       <Footer className="" />
